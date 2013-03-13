f12a3b68250f0b02f3f6a0479fcd3ae655308432
GZT <guozhitao@yahoo.cn>
2013-03-13 19:25:18 +0800

    test tool

---
+++ Autobots/TcpHttpTestTcpClientSimple/MainWindow.xaml.cs
@@ -0,0 +1,498 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TcpHttpTestTcpClient
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private bool _receivingData = false;
+
+		private Socket _client = null;
+
+		private string _serverIPAddress = "127.0.0.1";
+		public string ServerIPAddress
+		{
+			get
+			{
+				return _serverIPAddress;
+			}
+			set
+			{
+				_serverIPAddress = value;
+				NotifyPropertyChanged("ServerIPAddress");
+			}
+		}
+
+        private string _serverPort = "6000";
+        public string ServerPort
+        {
+            get
+            {
+                return _serverPort;
+            }
+            set
+            {
+                _serverPort = value;
+                NotifyPropertyChanged("ServerPort");
+            }
+        }
+
+		private string _textForSend = "";
+		public string TextForSend
+		{
+			get
+			{
+				return _textForSend;
+			}
+			set
+			{
+				_textForSend = value;
+				NotifyPropertyChanged("TextForSend");
+			}
+		}
+
+		private bool _inRun = false;
+		public bool InRun
+		{
+			get
+			{
+				return _inRun;
+			}
+			set
+			{
+				_inRun = value;
+				NotifyPropertyChanged("InRun");
+				NotifyPropertyChanged("NotInRun");
+			}
+		}
+
+		public bool NotInRun
+		{
+			get
+			{
+				return !_inRun;
+			}
+		}
+
+		private bool _isConnected = false;
+		public bool IsConnected
+		{
+			get
+			{
+				return _isConnected;
+			}
+			set
+			{
+				_isConnected = value;
+				NotifyPropertyChanged("IsConnected");
+				NotifyPropertyChanged("IsNotConnected");
+			}
+		}
+
+		public bool IsNotConnected
+		{
+			get
+			{
+				return !_isConnected;
+			}
+		}
+
+		private ObservableCollection<SentReceivedItem> _sentReceivedOc = new ObservableCollection<SentReceivedItem>();
+		public ObservableCollection<SentReceivedItem> SentReceivedOc
+		{
+			get
+			{
+				return _sentReceivedOc;
+			}
+		}
+
+        private object _objLock = new object();
+        public object ObjLock
+        {
+            get
+            {
+                return _objLock;
+            }
+        }
+
+		/// <summary>
+		/// Never get/set this value because it is used for UI and please use LogIsAutoScrolling
+		/// </summary>
+		private bool? _logAutoScrollingEnabled = true;
+		/// <summary>
+		/// Never get/set this value because it is used for UI and please use LogIsAutoScrolling
+		/// </summary>
+		public bool? LogAutoScrollingEnabled
+		{
+			get
+			{
+				return _logAutoScrollingEnabled;
+			}
+			set
+			{
+				if (value == null)
+					_logAutoScrollingEnabled = false;
+				else
+					_logAutoScrollingEnabled = value;
+				NotifyPropertyChanged("LogAutoScrollingEnabled");
+			}
+		}
+
+		public bool LogIsAutoScrolling
+		{
+			get
+			{
+				if (LogAutoScrollingEnabled == null)
+					return false;
+				else
+					return (bool)LogAutoScrollingEnabled;
+			}
+		}
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			DataContext = this;
+			dgSentReceived.DataContext = SentReceivedOc;
+            SentReceivedOc.CollectionChanged += new NotifyCollectionChangedEventHandler(SentReceivedOc_CollectionChanged);
+
+			ReceiveData_CheckBox_CheckedUnchecked(chkboxReceive, null);
+        }
+
+        private void SentReceivedOc_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+			lock (_objLock)
+			{
+				if (LogIsAutoScrolling == false || SentReceivedOc.Count < 1)
+					return;
+				
+				if (SentReceivedOc.Count < 1)
+                    return;
+                var border = VisualTreeHelper.GetChild(dgSentReceived, 0) as Decorator;
+                if (border != null)
+                {
+                    var scroll = border.Child as ScrollViewer;
+                    if (scroll != null) scroll.ScrollToEnd();
+                }
+            }
+        }
+
+		private void Send_ButtonClick(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrWhiteSpace(TextForSend))
+			{
+				PostLog("Cannot send empty data...");
+				return;
+			}
+
+			InRun = true;
+
+			Thread th = new Thread(new ThreadStart(ClientThread));
+			th.Start();
+		}
+
+        private void ClientThread()
+        {
+            DoClientThread();
+        }
+
+		private void DoClientThread()
+		{
+			string receivedText = "";
+			try
+			{
+				_client.Send(Encoding.ASCII.GetBytes(TextForSend));
+				PostLog("Sent.");
+				if (_receivingData == true)
+				{
+					byte[] bytes = new byte[1024 * 16];
+					int length = 0;
+					if ((length = _client.Receive(bytes)) == 0)
+						PostLog("Haven't received any data.");
+					else
+					{
+						receivedText = System.Text.Encoding.ASCII.GetString(bytes, 0, length);
+						PostLog("Received \"" + receivedText + "\".", Brushes.Blue);
+					}
+				}
+				Dispatcher.Invoke((ThreadStart)delegate()
+				{
+					SentReceivedOc.Add(new SentReceivedItem()
+					{
+						Index = SentReceivedOc.Count.ToString(),
+						Sent = TextForSend,
+						Received = receivedText
+					});
+				}, null);
+			}
+			catch (Exception ex)
+			{
+				PostLog("communication error : " + ex.Message);
+			}
+			InRun = false;
+		}
+
+		private void Connect_Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				_client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				IPAddress local = IPAddress.Parse(ServerIPAddress);
+				int port = 5080;
+				if (int.TryParse(ServerPort, out port) == false)
+					port = 5080;
+				IPEndPoint iep = new IPEndPoint(local, port);
+				_client.ReceiveTimeout = 60000;
+				_client.Connect(iep);
+				if (!_client.Connected)
+				{
+					_client.Disconnect(false);
+					_client.Close();
+					_client.Dispose();
+					_client = null;
+					IsConnected = false;
+
+					PostLog("Cannot connect.");
+				}
+				else
+				{
+					IsConnected = true;
+
+					PostLog("Connected.");
+				}
+			}
+			catch (Exception ex)
+			{
+				PostLog("Cannot connect : " + ex.Message);
+				try
+				{
+					_client.Disconnect(false);
+				}
+				catch (Exception) { }
+				try
+				{
+					_client.Close();
+				}
+				catch (Exception) { }
+				try
+				{
+					_client.Dispose();
+				}
+				catch (Exception) { }
+				_client = null;
+				IsConnected = false;
+			}
+		}
+
+		private void Disconnect_Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				_client.Disconnect(false);
+				_client.Close();
+				_client.Dispose();
+			}
+			catch (Exception ex)
+			{
+				PostLog("Cannot disconnect and force disconnect : " + ex.Message);
+				try
+				{
+					_client.Disconnect(false);
+				}
+				catch (Exception) { }
+				try
+				{
+					_client.Close();
+				}
+				catch (Exception) { }
+				try
+				{
+					_client.Dispose();
+				}
+				catch (Exception) { }
+			}
+			_client = null;
+			IsConnected = false;
+
+			PostLog("Disconnected.");
+		}
+
+		private void PostLog(string msg)
+		{
+			PostLog(msg, Brushes.Black);
+		}
+
+        private void PostLog(string msg, SolidColorBrush scb)
+		{
+			//lock(_objLock)
+			{
+				Dispatcher.Invoke((ThreadStart)delegate()
+				{
+					if (fldocLog.Blocks.Count > 100)
+						fldocLog.Blocks.Remove(fldocLog.Blocks.FirstBlock);
+					Run rch = new Run(msg);
+					Paragraph pch = new Paragraph(rch);
+					pch.Foreground = scb;
+					fldocLog.Blocks.Add(pch);
+					if (LogIsAutoScrolling == true)
+						rtxtLog.ScrollToEnd();
+				}, null);
+			}
+		}
+
+		private void ClearQueue_ButtonClick(object sender, RoutedEventArgs e)
+		{
+			lock (_objLock)
+			{
+				SentReceivedOc.Clear();
+			}
+		}
+
+		private void ClearLog_ButtonClick(object sender, RoutedEventArgs e)
+		{
+			lock (_objLock)
+			{
+				fldocLog.Blocks.Clear();
+			}
+		}
+
+		private void ReceiveData_CheckBox_CheckedUnchecked(object sender, RoutedEventArgs e)
+		{
+			CheckBox cb = sender as CheckBox;
+			_receivingData = (cb.IsChecked == true);
+			cb.Content = (_receivingData) ? "Receiving Data" : "No Receiving";
+		}
+	}
+
+	public class INotifyPropertyChangedClass : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+
+	public class SentReceivedItem : INotifyPropertyChangedClass
+	{
+		public enum DataCorrectEnum
+		{
+			None,
+			Ok,
+			Error
+		}
+
+		private string _index = "";
+		public string Index
+		{
+			get
+			{
+				return _index;
+			}
+			set
+			{
+				_index = value;
+			}
+		}
+
+		private string _sent = "";
+		public string Sent
+		{
+			get
+			{
+				return _sent;
+			}
+			set
+			{
+				_sent = value;
+			}
+		}
+
+		private string _received = "";
+		public string Received
+		{
+			get
+			{
+				return _received;
+			}
+			set
+			{
+				_received = value;
+			}
+		}
+
+		private DataCorrectEnum _dataCorrect = DataCorrectEnum.None;
+		public DataCorrectEnum DataCorrect
+		{
+			get
+			{
+				return _dataCorrect;
+			}
+			set
+			{
+				_dataCorrect = value;
+				NotifyPropertyChanged("DataCorrect");
+				if (_dataCorrect == DataCorrectEnum.None)
+				{
+					DataCorrectImage = null;
+				}
+				else
+				{
+					DataCorrectImage = new BitmapImage();
+					DataCorrectImage.BeginInit();
+					if (_dataCorrect == DataCorrectEnum.Ok)
+						DataCorrectImage.UriSource = new Uri("pack://application:,,,/TcpHttpTestTcpClient;component/resources/status_ok.png");
+					else
+						DataCorrectImage.UriSource = new Uri("pack://application:,,,/TcpHttpTestTcpClient;component/resources/status_error.png");
+					DataCorrectImage.EndInit();
+				}
+				NotifyPropertyChanged("DataCorrectImage");
+			}
+		}
+
+		private BitmapImage _dataCorrectImage = null;
+		public BitmapImage DataCorrectImage
+		{
+			get
+			{
+				return _dataCorrectImage;
+			}
+			set
+			{
+				_dataCorrectImage = value;
+				NotifyPropertyChanged("DataCorrectImage");
+			}
+		}
+	}
+}
